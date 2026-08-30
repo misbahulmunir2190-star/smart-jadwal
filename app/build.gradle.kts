708d1e31108d6ea1873c095bf2e57bdcb963033b
misbahulmunir2190-star <misbahulmunir2190@gmail.com>
2026-08-30 17:03:51 +0700

    Fix indentation in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ android {
       signingConfig = signingConfigs.getByName("release")
     }
     debug {
-  }
+    }
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
